a24d40f8c4d30fce2c2c92d981100eb3abcd35ac
Marcin Kondrat <marcin.kondrat@assts.tech>
2023-12-24 20:09:42 +0100

    build pod GH Pages
    to też trzeba w przyszłości zamienić na czytanie dev env

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export',
-    //assetPrefix: 'https://knio.assts.tech/',
+    output: 'export',
+    assetPrefix: 'https://knio.assts.tech/',
 };
 
 module.exports = nextConfig;
